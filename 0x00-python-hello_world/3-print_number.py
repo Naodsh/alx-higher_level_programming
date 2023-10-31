1a57950427e90367d0aebc23add3c373ec7fc54e
Naodsh <naodadanech1977@gmail.com>
2023-10-31 05:03:13 -0700

    edit 3-print_number

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number} Battery street") if isinstance(number, int) else print("Error: 'number' is not an integer.")
-
+print(f"{number} Battery street" if isinstance(number, int) else "Error: 'number' is not an integer.")
