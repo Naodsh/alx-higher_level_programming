32bf790c33e777ea021fd98e1f1c0932dd7236fc
Naodsh <naodadanech1977@gmail.com>
2023-11-01 07:34:53 -0700

    100 edited 2nd

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    print(chr(i) if (i - ord('z')) % 2 == 0 else chr(i).upper(), end="")
-
+    char = chr(i) if (i - ord('z')) % 2 == 0 else chr(i).upper()
+    print("{}".format(char), end="")
