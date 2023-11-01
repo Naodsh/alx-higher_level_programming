409a747979bad61557f4a40b98e1f4e9122b2022
Naodsh <naodadanech1977@gmail.com>
2023-11-01 07:11:16 -0700

    100 edit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    print("{}".format(chr(i)), end=('' if i % 2 == 1 else ''))
+for i in range(ord('z'), ord('a') - 1, -1):
+    print(chr(i) if (i - ord('z')) % 2 == 0 else chr(i).upper(), end="")
+
