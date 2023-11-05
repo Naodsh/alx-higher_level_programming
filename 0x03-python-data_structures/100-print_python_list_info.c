eceaacf6e487b4cf8952d91eaa8fffcfa457f843
Naodsh <naodadanech1977@gmail.com>
2023-11-05 07:19:13 -0800

    Advance task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include "Python.h"
+/**
+ * print_python_list_info - Prints information about a Python list.
+ * @p: Python list object
+ *
+ * Return: void
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, allocated, i;
+	yObject *item;
+
+	size = Py_SIZE(p);
+	allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
