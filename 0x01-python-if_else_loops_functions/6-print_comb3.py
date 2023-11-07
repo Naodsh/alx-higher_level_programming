7bff9fffc0b1487dbddb2b59906227e87e9c034d
Naodsh <naodadanech1977@gmail.com>
2023-11-07 10:12:19 -0800

    6-print_comb edit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}, ".format(i, j), end='')
-
-print("{:d}{:d}".format(9, 8))
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j), end='\n')
+        else:
+            print("{:d}{:d}, ".format(i, j), end="")
