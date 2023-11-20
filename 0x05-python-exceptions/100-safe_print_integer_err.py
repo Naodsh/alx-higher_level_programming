8ad4fc6154c63f3aebfc6c61caf49df90fc47856
Naodsh <naodadanech1977@gmail.com>
2023-11-20 05:00:51 -0800

    correction for 100 2nd time

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,6 +8,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(integer_value))
         return True
     except (ValueError, TypeError):
-        print("Exception: Unknown format code 'd' \
-                for object of type 'str'", file=sys.stderr)
+        print("Exception: Unknown format code\
+'d' for object of type 'str'", file=sys.stderr)
         return False
