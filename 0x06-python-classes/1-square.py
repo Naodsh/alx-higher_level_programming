2acafe1d8ba3361076c53e80076157fcf39afeb7
Naodsh <naodadanech1977@gmail.com>
2023-11-21 13:39:59 -0800

    style

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,16 @@
-#!/usr/bin/pythone
+#!/usr/bin/python3
+
 class Square:
+    """A class to represent a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """Initializes a Square instance with a specific size.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
