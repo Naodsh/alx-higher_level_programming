f99b70f126149b9178b7bed79876e61cf48c0702
Naodsh <naodadanech1977@gmail.com>
2023-11-22 02:04:26 -0800

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,50 @@
 #!/usr/bin/python3
+
+"""A class representing a square."""
+
+
 class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
     def __init__(self, size=0):
-        self.__size = size
+        """
+        Initializes a Square instance with an optional size.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.size = size
 
     @property
     def size(self):
+        """
+        Getter method to retrieve the size attribute.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Setter method to set the size attribute.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -17,9 +53,21 @@ class Square:
             self.__size = value
 
     def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        Prints the square using '#' character.
+
+        Prints:
+            The square represented by '#' characters.
+        """
         if self.__size == 0:
             print()
         else:
