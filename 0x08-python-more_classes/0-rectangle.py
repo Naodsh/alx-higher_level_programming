a485def5be96a472f97e11a70f6b4873d6ce76bb
Naodsh <naodadanech1977@gmail.com>
2023-11-27 11:31:05 -0800

    README.md 0-rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """This is a simple class representing a rectangle.
+
+    Attributes:
+        No attributes are defined for this empty Rectangle class.
+
+    Methods:
+        No methods are defined for this empty Rectangle class.
+    """
+    pass
