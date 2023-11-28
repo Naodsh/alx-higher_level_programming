c5c7dfc7a39ebf06e664e89e89624d194f39a0ce
Naodsh <naodadanech1977@gmail.com>
2023-11-28 12:13:37 -0800

    edit 101-locked_class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -10,12 +10,19 @@ class LockedClass:
         - value: The value being assigned to the attribute.
         """
 
-        # Check if the attribute is not 'first_name'
-        # and 'first_name' has not been set yet
-        if not hasattr(self, 'first_name') and name != 'first_name':
-            # If conditions are met, raise an AttributeError
-            raise AttributeError(f"'LockedClass' object has no attribute \
-                    '{name}'")
-        else:
-            # Otherwise, allow setting the attribute
-            self.__dict__[name] = value
+        if name != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute \
+                    '{}'".format(name))
+
+    def __delattr__(self, name):
+        """
+        Prevents the dynamic creation of attributes except for 'first_name'.
+
+        Args:
+        - self: The instance of the class.
+        - name: The name of the attribute being set.
+        """
+
+        if name != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute \
+                    '{}'".format(name))
