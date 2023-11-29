8b5f0efcef660d2b51738c108ba655acf81f4dde
Naodsh <naodadanech1977@gmail.com>
2023-11-29 06:08:26 -0800

    3-recangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -79,8 +79,9 @@ class Rectangle:
         Returns:
             int: The perimeter of the rectangle.
         """
-        return 0 if self.__width == 0 or self.__height == 0 else \
-            2 * (self.__width + self.__height)
+        if self.__width == 0 or self.__heith == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
 
     def __str__(self):
         """Returns a string representation of the rectangle.
