e885dbdbcb1d8168e3bf359c82a46febbcba5c1d
Naodsh <naodadanech1977@gmail.com>
2023-12-03 03:46:55 -0800

    the rest of the tasks

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,4 +1,4 @@
-#!/usr/binpython3
+#!/usr/bin/python3
 """Rectangle Module """
 
 
@@ -18,20 +18,12 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        """
-        Initializes a Rectangle instance with a specific width and height.
-
-        Args:
-            width (int, optional): The width of the rectangle. Defaults to 0.
-            height (int, optional): The height of the rectangle. Defaults to 0.
-        """
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        """int: The width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -45,7 +37,6 @@ class Rectangle:
 
     @property
     def height(self):
-        """int: The height of the rectangle."""
         return self.__height
 
     @height.setter
@@ -58,48 +49,21 @@ class Rectangle:
             self.__height = value
 
     def area(self):
-        """
-        Calculates the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
         return self.__width * self.__height
 
     def perimeter(self):
-        """
-        Calculates the perimeter of the rectangle.
-
-        Returns:
-            int: The perimeter of the rectangle.
-        """
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """
-        Returns a string representation of the rectangle.
-
-        Returns:
-            str: The rectangle represented by '#' characters.
-        """
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width] * self.__height)
 
     def __repr__(self):
-        """
-        Returns a string representation can be used to recreate the instance.
-
-        Returns:
-            str: String representation to recreate the rectangle instance.
-        """
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
-        """
-        Deletes the Rectangle instance and decrements the number of instances.
-        """
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
