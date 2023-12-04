312619a0431649662f56a3078bfb157401067d32
Naodsh <naodadanech1977@gmail.com>
2023-12-04 04:30:50 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,11 @@
 class MyList(list):
     """Inherits from list and adds a print_sorted method."""
 
+    def __init__(self, *args):
+        """Initialize MyList with optional initial elements."""
+        super().__init__(args)
+
     def print_sorted(self):
         """Prints the list in ascending order."""
+
         print(sorted(self))
