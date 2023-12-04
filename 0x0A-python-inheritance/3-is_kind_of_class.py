e0a8e0a701264da13ec8fbc148fcbc80e2a95f8b
Naodsh <naodadanech1977@gmail.com>
2023-12-04 11:20:57 -0800

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module of is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns True if obj is an instance of, or if obj is an instance
+    of a class that inherited from, the specified class; otherwise False.
+    """
+    return isinstance(obj, a_class)
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module of is_inherits_from class"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False.
+    """
+    return issubclass(type(obj), a_class)
