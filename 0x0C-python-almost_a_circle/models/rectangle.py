05c4ef92460788e1b556e91ca678e1b5e139d767
Naodsh <naodadanech1977@gmail.com>
2023-12-09 02:00:31 -0800

    rectangle final

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,9 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """ Rectangle Class """
+
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
@@ -19,6 +22,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if isinstance(value int):
+            rise TypeError("width must be an integer")
+        if value <= 0:
+            rise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -27,6 +34,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if isinstance(value int):
+            rise TypeError("height  must be an integer")
+        if value <= 0:
+            rise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -35,6 +46,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if isinstance(value int):
+            rise TypeError("x  must be an integer")
+        if value <= 0:
+            rise ValueError("x must be > 0")
         self.__x = value
 
     @property
@@ -43,4 +58,30 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if isinstance(value int):
+            rise TypeError("y  must be an integer")
+        if value <= 0:
+            rise ValueError("y must be > 0")
         self.__y = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def display(self):
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        return "[Rectangle] {} {}/{} - {}/{}" .format(
+                self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
