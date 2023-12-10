27b2797a6c900ea5629dbae029d69f49651aa755
Naodsh <naodadanech1977@gmail.com>
2023-12-10 06:24:29 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,17 +6,42 @@ import sys
 
 
 def print_stats(file_sizes, status_codes):
-    """ Function to print """
     print(f"File size: {sum(file_sizes)}")
     for code in sorted(status_codes):
         print(f"{code}: {status_codes[code]}")
 
 
 def parse_line(line):
-    """ Function that reads stdin line by line and parse """
     parts = line.split()
     if len(parts) > 2:
         file_size = int(parts[-1])
         status_code = parts[-2]
         return file_size, status_code
     return None, None
+
+
+def main():
+    file_sizes = []
+    status_codes = {}
+
+    try:
+        line_count = 0
+        for line in sys.stdin:
+            line_count += 1
+            file_size, status_code = parse_line(line)
+            if file_size is not None and status_code is not None:
+                file_sizes.append(file_size)
+                status_codes[status_code] = status_codes.get(
+                        status_code, 0) + 1
+
+            if line_count == 10:
+                print_stats(file_sizes, status_codes)
+                line_count = 0
+
+    except KeyboardInterrupt:
+        print_stats(file_sizes, status_codes)
+        raise
+
+
+if __name__ == "__main__":
+    main()
