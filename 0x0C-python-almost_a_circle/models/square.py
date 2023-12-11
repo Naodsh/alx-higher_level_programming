a77f0dce8342280e9bbdc34f5ea820c25e07e68f
Naodsh <naodadanech1977@gmail.com>
2023-12-11 14:04:04 -0800

    square re do

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,17 +6,12 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ Square Class inherits from Rectangle """
     def __init__(self, size, x=0, y=0, id=None):
+        """ init function """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """ str function """
         return "[Square] ({}) {}/{} - {}" .format(
                 self.id, self.x, self.y, self.width)
-
-    def to_dictionary(self):
-        return {
-            'id': self.id,
-            'size': self.width,
-            'x': self.x,
-            'y': self.y
-        }
