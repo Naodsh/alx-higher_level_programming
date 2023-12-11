870e1f33e2c80209971c09d5786038d40fc16a84
Naodsh <naodadanech1977@gmail.com>
2023-12-11 13:35:09 -0800

    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,29 +9,9 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
-
-    def update(self, *args, **kwargs):
-        if args:
-            attrs = ["id", "size", "x", "y"]
-            for i, arg in enumerate(args):
-                setattr(self, attrs[i], arg)
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
     def __str__(self):
         return "[Square] ({}) {}/{} - {}" .format(
-                self.id, self.x, self.y, self.size)
-
-        # return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+                self.id, self.x, self.y, self.width)
 
     def to_dictionary(self):
         return {
