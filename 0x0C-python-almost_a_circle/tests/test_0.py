020db4a8abb3a41ee724d0bb421ca1516078f920
Naodsh <naodadanech1977@gmail.com>
2023-12-11 10:25:25 -0800

    ./tests/test_0.py

--- 0x0C-python-almost_a_circle/tests/test_0.py
+++ 0x0C-python-almost_a_circle/tests/test_0.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
 import unittest
+from models.base import Base
+from models.rectangle import Rectangle
 
+class TestBase(unittest.TestCase):
+    def test_base_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
 
-class TestStringMethods(unittest.TestCase):
+class TestRectangle(unittest.TestCase):
+    def test_rectangle_id(self):
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 5)
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 6)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
-
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
-
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
