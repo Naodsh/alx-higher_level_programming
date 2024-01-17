9e5b85f1854bab8ded9d706a9cc054362eb59f95
Naodsh <naodadanech1977@gmail.com>
2024-01-17 15:43:51 -0800

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,5 @@
 -- This script lists all shows and genres linked to each show in the hbtn_0d_tvshows database
 
--- Specify the database name as a command-line argument
-USE `hbtn_0d_tvshows`;
-
 -- List all shows and genres
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
