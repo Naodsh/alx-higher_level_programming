34065c2ba26b4dae21cffe6e28d29fa272480c35
Naodsh <naodadanech1977@gmail.com>
2024-01-17 13:29:38 -0800

    task  3, 4, 5

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,7 @@
+-- This script lists all tables of a specified database
+
+-- Specify the database name as a command-line argument
+USE `mysql`;
+
+-- Show tables in the specified database
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+-- This script prints the full description of the table first_table
+
+-- Specify the database name as a command-line argument
+USE `hbtn_0c_0`;
+
+-- Show the full description of the first_table
+DESCRIBE first_table;
