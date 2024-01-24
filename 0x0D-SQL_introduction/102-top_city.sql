22eec2dfdc6a6f6acf4c8591a026c619b2ef2d25
Naodsh <naodadanech1977@gmail.com>
2024-01-24 07:19:46 -0800

    102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Calculate and display the top 3 cities by average temperature during July and August
-SELECT city, ROUND(AVG(temperature), 4) AS avg_temp
-FROM temperatures
+SELECT `city`, ROUND(AVG(`temperature`), 4) AS `avg_temp`
+FROM `temperatures`
 WHERE MONTH(date) IN (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
 LIMIT 3;
