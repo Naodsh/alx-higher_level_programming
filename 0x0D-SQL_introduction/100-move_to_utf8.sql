02661f88c9c7743fc57ee7088c0158b0135dac22
Naodsh <naodadanech1977@gmail.com>
2024-01-24 04:56:22 -0800

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,10 +4,3 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Modify the table and field to utf8mb4
 ALTER TABLE hbtn_0c_0.first_table
     CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Use the hbtn_0c_0 database
-USE hbtn_0c_0;
-
--- Modify the field in first_table to utf8mb4
-ALTER TABLE first_table
-    MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
