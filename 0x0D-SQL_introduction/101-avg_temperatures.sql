b5329952398e9527ce2714feee3a511698fec70a
Naodsh <naodadanech1977@gmail.com>
2024-01-24 07:05:28 -0800

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Calculate and display the average temperature by city, ordered by temperature (descending)
-SELECT city, ROUND(AVG(temperature), 4) AS avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
