0413a93a150e61e584314e2d2794abe580fa594d
Naodsh <naodadanech1977@gmail.com>
2024-02-13 01:50:10 -0800

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-const myVar = "JavaScript is amazing";
+// Declare a constant array with the three strings
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-console.log(myVar);
+// Loop through the array and print each element using console.log
+for (const message of messages) {
+  console.log(message);
+}
