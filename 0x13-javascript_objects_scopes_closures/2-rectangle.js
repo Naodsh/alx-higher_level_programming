3b71f2d353232ed8d84d88ef84b7d7629b179f2c
Naodsh <naodadanech1977@gmail.com>
2024-02-13 10:45:33 -0800

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,9 +3,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      // Create an empty object
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
