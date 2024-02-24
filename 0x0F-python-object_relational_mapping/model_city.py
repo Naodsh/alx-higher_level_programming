a90fc3defaa15ae3e5cd0b3057f3c2a6f00e6c3c
Naodsh <naodadanech1977@gmail.com>
2024-02-24 05:27:51 -0800

    model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,26 +3,16 @@
 write a script 14-model_city_fetch_by_state.py that prints all
 City objects from the database hbtn_0e_14_usa:
 """
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State, City
-import sys
 
-if __name__ == "__main__":
-    # Get MySQL credentials from command line arguments
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 
-    # Create an engine to connect to the MySQL server
-    engine = create_engine(
-            f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}")
 
-    # Create a session to interact with the database
-    Session = sessionmaker(bind=engine)
-    session = Session()
+class City(Base):
 
-    # Query all City objects and sort by ID
-    cities = session.query(City).order_by(City.id).all()
-
-    # Print City objects in the specified format
-    for city in cities:
-        print(f"{city.state.name}: ({city.id}) {city.name}")
+    """ city class inherited from Base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
