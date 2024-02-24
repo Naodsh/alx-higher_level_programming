bf8567c8674e6594c3594bc25ae8e0731bfa2d82
Naodsh <naodadanech1977@gmail.com>
2024-02-24 05:47:11 -0800

    model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,7 +6,7 @@ City objects from the database hbtn_0e_14_usa:
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
+from model_state import Base, State
 
 
 class City(Base):
