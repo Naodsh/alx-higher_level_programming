1ef7bb28ed4ad43fb78fd172b2003dbb9d0d4ec6
Naodsh <naodadanech1977@gmail.com>
2024-02-24 03:32:44 -0800

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" a script that lists all State objects from the database hbtn_0e_6_usa """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
