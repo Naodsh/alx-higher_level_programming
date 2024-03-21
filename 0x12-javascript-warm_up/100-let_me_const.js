d0716ec7119a00b0f11b6920cecfec3f15753bef
Naodsh <naodadanech1977@gmail.com>
2024-03-21 09:50:42 -0700

    final

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// Modifies the value of myVar to 333
+global.myVar = 333;
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Define a function with the name add that takes two parameters
+function add (a, b) {
+  // Return the sum of the two parameters
+  return a + b;
+}
+
+// Export the function using the module.exports object
+module.exports.add = add;
