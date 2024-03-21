b537e7d6d19f282ec30d84eb0320c396ac3864de
Naodsh <naodadanech1977@gmail.com>
2024-03-21 05:38:27 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-a Python file similar to model_state.py named model_city.py that contains
-the class definition of a City.
+script that prints all City objects from the database hbtn_0e_14_usa
 """
+
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from model_city import Base, City
-import sys
+from model_city import City
+
 
 if __name__ == "__main__":
     # Get MySQL credentials from command line arguments
@@ -17,13 +18,18 @@ if __name__ == "__main__":
     engine = create_engine(
             f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}")
 
+    # Bind the engine to the metadata of the Base class so that the
+    # declaratives can be accessed through a DBSession instance
+    Base.metadata.create_all(engine)
+
     # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query all City objects and sort by ID
-    cities = session.query(City).order_by(City.id).all()
+    # Query all City objects joined with State and sort by City ID
+    cities = session.query(City, State).filter(
+            City.state_id == State.id).order_by(City.id).all()
 
     # Print City objects in the specified format
-    for city in cities:
-        print(f"{city.name}: ({city.id}) {city.name}")
+    for city, state in cities:
+        print(f"{state.name}: ({city.id}) {city.name}")
