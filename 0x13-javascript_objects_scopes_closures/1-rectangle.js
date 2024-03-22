e96995dca3251991bd2f537ff40ea29ac5ac7961
Naodsh <naodadanech1977@gmail.com>
2024-03-22 09:12:15 -0700

    up to 4

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// rectangle class with width and height
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
