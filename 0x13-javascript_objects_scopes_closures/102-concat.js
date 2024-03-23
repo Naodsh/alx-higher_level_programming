ac349c1ad053f93239f9e6193a0cc2c7da6ed250
Naodsh <naodadanech1977@gmail.com>
2024-03-23 06:32:49 -0700

    102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+const fileContent1 = fs.readFileSync(sourceFile1, 'utf8');
+const fileContent2 = fs.readFileSync(sourceFile2, 'utf8');
+
+const concatenatedContent = fileContent1 + '\n' + fileContent2;
+
+fs.writeFileSync(destinationFile, concatenatedContent);
+
