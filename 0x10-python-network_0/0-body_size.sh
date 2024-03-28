c9075990b1f4da972e40b30dea9c34708e2eea2b
Naodsh <naodadanech1977@gmail.com>
2024-03-28 13:58:22 -0700

    0-body_size.sh rewrite

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
+# This script takes a URL as argument, sends a request to that URL, and displays the size of the body of the response in bytes
+# It uses curl with the -s (silent mode) option
 
-# Check if URL is provided as argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request using curl and store response body in a variable
-# response=$("curl -sI $1 | grep -i 'Content-Length' | awk '{print $2}'")
-response=$(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}')
-# If Content-Length header is present, print its value (size of body), otherwise print 0
-if [ -n "$response" ]; then
-    echo "$response"
-else
-    echo "0"
-fi
+curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
