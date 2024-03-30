544453f41982b95a8686e9bc8faf1e2354a15452
Naodsh <naodadanech1977@gmail.com>
2024-03-30 06:00:23 -0700

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -24,19 +24,18 @@ def find_peak(list_of_integers):
     """
     if not list_of_integers:
         return None
-    
+
     low = 0
     high = len(list_of_integers) - 1
-    
     while low < high:
         mid = (low + high) // 2
         if list_of_integers[mid] < list_of_integers[mid + 1]:
             low = mid + 1
         else:
             high = mid
-            
+
     return list_of_integers[low]
 
-# Testing the function
+
 if __name__ == "__main__":
     find_peak(list_of_integers)
