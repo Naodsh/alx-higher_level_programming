0c7d57dcd5401a0b076929590f43e196ec596e38
Naodsh <naodadanech@1977>
2024-06-09 03:57:46 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,23 +2,25 @@
 
 const request = require('request');
 const apiUrl = process.argv[2];
-const wedgeAntillesId = 18;
+const wedgeAntillesId = '18';
 
 request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error(error);
   } else if (response.statusCode === 200) {
     const films = JSON.parse(body).results;
     let count = 0;
 
     films.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)) {
-        count++;
-      }
+      film.characters.forEach(character => {
+        if (character.includes(`/${wedgeAntillesId}/`)) {
+          count++;
+        }
+      });
     });
 
     console.log(count);
   } else {
-    console.log(`Error: ${response.statusCode}`);
+    console.error(`Error: ${response.statusCode}`);
   }
 });
